ad9c24b03ae3823df95f8884b9c9cd98c3c3b9c3
YunHaesu <69497806+YunHaesu@users.noreply.github.com>
2020-11-17 15:25:28 +0900

    Login

--- model2test/src/controller/BoardFrontController.java
+++ model2test/src/controller/BoardFrontController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import action.Action;
+import action.BoardListAction;
 import action.BoardWriteProAction;
 import vo.ActionForward;
 
@@ -46,6 +47,13 @@ public class BoardFrontController extends HttpServlet {
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
+		}else if(command.equals("/boardList.do")){
+			action = new BoardListAction();
+			try {
+			forward = action.execute(request, response);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		
 		if(forward != null) {
@@ -56,5 +64,7 @@ public class BoardFrontController extends HttpServlet {
 				rd.forward(request, response);
 			}
 		}
+		
+		
 	}
 }
--- model2test/src/dao/BoardDAO.java
+++ model2test/src/dao/BoardDAO.java
@@ -15,31 +15,32 @@ public class BoardDAO {
 	DataSource ds;
 	Connection conn;
 	private static BoardDAO boardDAO;
-	
+
 	private BoardDAO() {
 	}
-	
+
 	public static BoardDAO getInstance() {
-		if(boardDAO == null) boardDAO = new BoardDAO();
+		if (boardDAO == null)
+			boardDAO = new BoardDAO();
 		return boardDAO;
 	}
-	
+
 	public void setConnection(Connection conn) {
 		this.conn = conn;
 	}
-	
+
 	public int selectListCount() {
 		int listCount = 0;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		
+
 		try {
 			pstmt = conn.prepareStatement("select count(*) from board");
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				listCount = rs.getInt(1);
-				
+
 			}
 		} catch (Exception e) {
 			System.out.println("getListCount 에러: " + e);
@@ -47,24 +48,25 @@ public class BoardDAO {
 			close(rs);
 			close(pstmt);
 		}
-		
+
 		return listCount;
 	}
-	
+
 	public ArrayList<BoardBean> selectArticleList(int page, int limit) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String board_list_sql = "select * from board order by board_re_ref desc, board_re_seq asc limit ?, 10";
+		String board_list_sql = "select * from board order by board_re_ref desc, board_re_seq asc limit ?, ?";
 		ArrayList<BoardBean> articleList = new ArrayList<BoardBean>();
 		BoardBean board = null;
 		int startRow = (page - 1) * 10;
-		
+
 		try {
 			pstmt = conn.prepareStatement(board_list_sql);
 			pstmt.setInt(1, startRow);
+			pstmt.setInt(2, limit);
 			rs = pstmt.executeQuery();
-			
-			while(rs.next()) {
+
+			while (rs.next()) {
 				board = new BoardBean();
 				board.setBoard_num(rs.getInt("board_num"));
 				board.setBoard_name(rs.getString("board_name"));
@@ -84,20 +86,20 @@ public class BoardDAO {
 			close(rs);
 			close(pstmt);
 		}
-		
+
 		return articleList;
 	}
-	
+
 	public BoardBean selectArticle(int board_num) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		BoardBean boardBean = null;
-		
+
 		try {
 			pstmt = conn.prepareStatement("select * from board where board_num = ?");
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				boardBean = new BoardBean();
 				boardBean.setBoard_num(rs.getInt("board_num"));
 				boardBean.setBoard_name(rs.getString("board_name"));
@@ -110,45 +112,47 @@ public class BoardDAO {
 				boardBean.setBoard_readcount(rs.getInt("board_readcount"));
 				boardBean.setBoard_date(rs.getDate("board_date"));
 			}
-		}catch (Exception e) {
+		} catch (Exception e) {
 			System.out.println("getDetail 에러: " + e);
 		} finally {
 			close(rs);
 			close(pstmt);
 		}
-		
+
 		return boardBean;
 	}
-	
+
 	public int insertArticle(BoardBean article) {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		int num = 0;
 		String sql = "";
 		int insertCount = 0;
-		
+
 		try {
 			pstmt = conn.prepareStatement("select max(board_num) from board");
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) num = rs.getInt(1) + 1;
-			else num = 1;
-			
+
+			if (rs.next())
+				num = rs.getInt(1) + 1;
+			else
+				num = 1;
+
 			sql = "insert into board values (?,?,?,?,?,?,?,?,?,?,now())";
 //			close(rs);
 //			close(pstmt);
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1,  num);
+			pstmt.setInt(1, num);
 			pstmt.setString(2, article.getBoard_name());
 			pstmt.setString(3, article.getBoard_pass());
 			pstmt.setString(4, article.getBoard_subject());
 			pstmt.setString(5, article.getBoard_content());
 			pstmt.setString(6, article.getBoard_file());
-			pstmt.setInt(7,  num);
-			pstmt.setInt(8,  0);
-			pstmt.setInt(9,  0);
-			pstmt.setInt(10,  0);
-			
+			pstmt.setInt(7, num);
+			pstmt.setInt(8, 0);
+			pstmt.setInt(9, 0);
+			pstmt.setInt(10, 0);
+
 			insertCount = pstmt.executeUpdate();
 		} catch (Exception e) {
 			System.out.println("boardInsert 에러: " + e);
@@ -156,7 +160,7 @@ public class BoardDAO {
 			close(rs);
 			close(pstmt);
 		}
-		
+
 		return insertCount;
 	}
 }
